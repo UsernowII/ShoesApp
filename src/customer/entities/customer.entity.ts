6dce53b27d4bbadeecad1f39233e276d9e82469f
Usernow <usernow.82@gmail.com>
2022-08-20 14:34:27 -0500

    Sync branch

--- src/customer/entities/customer.entity.ts
+++ src/customer/entities/customer.entity.ts
@@ -1,5 +1,6 @@
 import { Column, Entity, JoinColumn, OneToOne } from "typeorm";
 import { BaseEntity } from "../../config/base.entity"
+import { UserEntity } from '../../user/entities/user.entity';
 
 @Entity({name : "customers"})
 export class CustomerEntity extends BaseEntity{
@@ -10,7 +11,8 @@ export class CustomerEntity extends BaseEntity{
     @Column()
     dni! : number;
 
-    @OneToOne()
-    @JoinColumn({name : "user_id"}); 
+    @OneToOne( ()=> UserEntity, (user) => user.customer)
+    @JoinColumn({name : "user_id"})
+    customer! : UserEntity 
 
 }
