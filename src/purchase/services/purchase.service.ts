0d4ed585168956241d40ce421a589c2fad6a5932
Usernow <usernow.82@gmail.com>
2022-08-27 14:36:11 -0500

    Services for each domain

---
+++ src/purchase/services/purchase.service.ts
@@ -0,0 +1,37 @@
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { BaseService } from '../../config/base.service';
+import { PurchaseEntity } from '../entities/purchase.entity';
+import { PurchaseDTO } from '../dto/purchase.dto';
+
+
+export class PurchaseService extends BaseService <PurchaseEntity> {
+
+    constructor(){
+        super(PurchaseEntity);
+    }
+
+    async findAllPurchases(): Promise< PurchaseEntity[]>{
+        return await (await this.execRepository).find();
+    }
+
+    async findPurchaseById(id: string): Promise<PurchaseEntity | null> {
+        return (await this.execRepository).findOneBy({ id });
+    }
+
+    async createPurchase(body: PurchaseDTO): Promise<PurchaseEntity> {
+        return (await this.execRepository).save(body);
+    }
+
+    async deletePurchase(id: string): Promise<DeleteResult> {
+        return (await this.execRepository).delete({ id });
+    }
+
+    async updatePurchase(
+    id: string,
+    infoUpdate: PurchaseDTO
+    ): Promise<UpdateResult> {
+        return (await this.execRepository).update(id, infoUpdate);
+    }
+
+
+}
